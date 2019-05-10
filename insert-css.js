0725c874d5293ac791f5a216ca351cef8fbd5b0f
Axel Leroy <blackeco@free.fr>
2019-05-10 13:49:36 +0200

    Fixed typo on manifest. Now applies CSS on new tabs.

--- insert-css.js
+++ insert-css.js
@@ -2,12 +2,11 @@ function onError(error) {
   console.log(`Error : ${error}`);
 }
 
-browser.tabs.query({url : "*://va.news-republic.com/*"}).then((tabs) => {
-  for (let tab of tabs) {
-    var displayFullContent = browser.tabs.insertCSS(tab.id, {
-      file: "custom-style.css"
-    });
-    displayFullContent.then(null, onError);
-  }
-}, onError);
-
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
+  var displayFullContent = browser.tabs.insertCSS(tabId, {
+    file: "custom-style.css"
+  });
+  displayFullContent.then(null, onError);
+}, {
+  urls: ["*://va.news-republic.com/*"]
+});
